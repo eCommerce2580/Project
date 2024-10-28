f7242f8a04c8ea0d681253699151b7dfa5ff7ce0
estiww <esti69771@gmail.com>
2024-10-28 14:33:49 +0200

    verify-email

--- my-app/app/store/types.ts
+++ my-app/app/store/types.ts
@@ -1,22 +1,31 @@
 // store/types.ts
+
+// User state type
 export interface UserState {
-    id: string | null;
-    name: string | null;
-    email: string | null;
-    address: string | null;
-    img: string | null;
-    isAuthenticated: boolean;
-  }
-  export interface CartState {
-    productId: string;
-    name: string;
-    price: number;
-    quantity: number;
-  }
-  
-  // הטיפוס עבור כל ה-state של האפליקציה (store)
-  export interface RootState {
-    user: UserState;
-    cart: CartState
-  }
-  
+  id: string | null;
+  name: string | null;
+  email: string | null;
+  address: string | null;
+  image: string | null;
+  isAuthenticated: boolean;
+  isVerified: boolean;  // הוספת isVerified ל-user state
+}
+
+// Cart item type
+export interface CartItem {
+  productId: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
+
+// Cart state type
+export interface CartState {
+  items: CartItem[]; // ריבוי פריטים בעגלת הקניות
+}
+
+// טיפוס לכל ה-state של האפליקציה (store)
+export interface RootState {
+  user: UserState;
+  cart: CartState;
+}
