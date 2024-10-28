36b3a6995d2f2c15f1d8bf3e06c98a9a07c3a0a2
SaraTaharani <taharani1517@gmail.com>
2024-10-28 15:26:06 +0200

    orders of user

--- my-app/app/store/types.ts
+++ my-app/app/store/types.ts
@@ -1,44 +1,58 @@
 // store/types.ts
 
 export interface UserState {
-    id: string | null;
-    name: string | null;
-    email: string | null;
-    address: string | null;
-    img: string | null;
-    isAuthenticated: boolean;
-  }
-  export interface CartState {
-    productId: string;
-    name: string;
-    price: number;
-    quantity: number;
-  }
-  
-  // הטיפוס עבור כל ה-state של האפליקציה (store)
-  export interface RootState {
-    user: UserState;
-    cart: CartState
-  }
-  
+  id: string | null;
+  name: string | null;
+  email: string | null;
+  address: string | null;
+  img: string | null;
+  isAuthenticated: boolean;
+}
+export interface CartState {
+  productId: string;
+  name: string;
+  price: number;
+  quantity: number;
+}
 
-  export interface Order {
-    id: String; 
-    orderDate: Date;
-    totalAmount: number;
-    paymentMethodName: String; 
-    shippingAddressId: String;
-    expectedDeliveryDate?: Date;
-    userId: String; 
-    statusName: String; 
-    orderItems: OrderItem[]; 
-  }
- 
-  export interface OrderItem {
-    id: String;
-    orderId: String;
-    productId: String; 
-    quantity: number;
-    price: number;
-  }
-  
+// הטיפוס עבור כל ה-state של האפליקציה (store)
+export interface RootState {
+  user: UserState;
+  cart: CartState
+}
+
+
+interface Product {
+  name: string;
+  price: number;
+};
+interface OrderItem {
+  id: string;
+  orderId: string;
+  productId: string;
+  quantity: number;
+  price: number;
+  product: Product;
+};
+
+interface PaymentMethod {
+  name: string;
+};
+
+interface OrderStatus {
+  name: string;
+};
+
+export interface Order {
+  id: string;
+  orderDate: Date;
+  totalAmount: number;
+  paymentMethodId: string;
+  shippingAddressId: string;
+  expectedDeliveryDate: Date;
+  userId: string;
+  statusId: string;
+  paymentMethod: PaymentMethod;
+  status: OrderStatus;
+  orderItems: OrderItem[];
+};
--- my-app/app/userOrders/page.tsx
+++ my-app/app/userOrders/page.tsx
@@ -1,47 +1,45 @@
 import prisma from "@/prisma/client";
-import { Order } from "../store/types";
 import { getServerSession } from "next-auth/next"
 import { authOptions } from "../api/auth/[...nextauth]/options";
+import { Order } from "../store/types";
 
 async function getUserOrders(id: any) {
     try {
-        const orders = await prisma.orders.findMany(
-            {
-                where: {
-                    userId: id,
+        const orders = await prisma.orders.findMany({
+            where: {
+                userId: "671f7be7abd58aaabe3e580a", 
+            },
+            include: {
+                paymentMethod: {
+                    select: {
+                        name: true, 
+                    },
                 },
-                include: {
-                    paymentMethod: {
-                        select: {
-                            name: true, 
-                        },
+                status: {
+                    select: {
+                        name: true, 
                     },
-                    status: {
-                        select: {
-                            name: true, 
-                        },
-                    },
-                    orderItems: {
-                        include: {
-                            product: {
-                                select: {
-                                    name: true,
-                                    price:true
-                                },
+                },
+                orderItems: {
+                    include: {
+                        product: {
+                            select: {
+                                name: true,
+                                price: true,
                             },
                         },
                     },
                 },
-            }
-        );
-        console.log("jfvbldkb")
-        console.log(orders);
+            },
+        });
+        console.log(JSON.stringify(orders, null, 2)); 
         return orders;
     } catch (error) {
         console.error("Error fetching orders:", error);
-        throw error; // רשות להשליך את השגיאה למעלה
+        throw error; 
     }
 }
+
 
 export default async function UserOrders() {
     const session = await getServerSession(authOptions)
@@ -63,26 +61,34 @@ export default async function UserOrders() {
                             <li className="font-medium text-lg leading-8 cursor-pointer text-black transition-all duration-500 hover:text-indigo-600">Cancelled</li>
                         </ul>
                     </div>
-                    {/* {orders.map((order:Order) => (<OrderComponent order={order} />))} */}
-                    <OrderComponent />
+                    {orders.map((order) => <OrderComponent />)}
+
+                    {/* {orders.map((order) => <OrderComponent order={order} />)} */}
                 </div>
             </section>
         </div>
     );
 }
 
-// function OrderComponent(order: Order) {
+
+
 function OrderComponent() {
-    return (
+        return (
         <div className="mt-7 border border-gray-300 pt-9">
             <div className="flex max-md:flex-col items-center justify-between px-3 md:px-11">
                 <div className="data">
-                    <p className="font-medium text-lg leading-8 text-black whitespace-nowrap">Order : #123</p>
-                    <p className="font-medium text-lg leading-8 text-black mt-3 whitespace-nowrap">Order Payment : 18th March 2021</p>
+                    {/* <p className="font-medium text-lg leading-8 text-black whitespace-nowrap">Order : #{order.id}</p> */}
+                    <p className="font-medium text-lg leading-8 text-black mt-3 whitespace-nowrap">
+                        {/* Order Payment : {new Date(order.orderDate).toLocaleDateString()} */}
+                    </p>
                 </div>
                 <div className="flex items-center gap-3 max-md:mt-5">
-                    <button className="rounded-full px-7 py-3 bg-white text-gray-900 border border-gray-300 font-semibold text-sm shadow-sm transition-all duration-500 hover:shadow-gray-200 hover:bg-gray-50 hover:border-gray-400">Show Invoice</button>
-                    <button className="rounded-full px-7 py-3 bg-indigo-600 shadow-sm text-white font-semibold text-sm transition-all duration-500 hover:shadow-indigo-400 hover:bg-indigo-700">Buy Now</button>
+                    <button className="rounded-full px-7 py-3 bg-white text-gray-900 border border-gray-300 font-semibold text-sm shadow-sm transition-all duration-500 hover:shadow-gray-200 hover:bg-gray-50 hover:border-gray-400">
+                        Show Invoice
+                    </button>
+                    <button className="rounded-full px-7 py-3 bg-indigo-600 shadow-sm text-white font-semibold text-sm transition-all duration-500 hover:shadow-indigo-400 hover:bg-indigo-700">
+                        Buy Now
+                    </button>
                 </div>
             </div>
             <ProductInOrder />
@@ -97,12 +103,47 @@ function OrderComponent() {
                         </button>
                         <p className="font-normal text-xl leading-8 text-gray-500 sm:pl-8">Payment Is Successful </p>
                     </div>
-                    <p className="font-medium text-xl leading-8 text-black max-sm:py-4"><span className="text-gray-500"> Total Price: </span>&nbsp;$200.00</p>
+                    <p className="font-medium text-xl leading-8 text-black max-sm:py-4">
+                        {/* <span className="text-gray-500"> Total Price: </span>&nbsp;${order.totalAmount.toFixed(2)} */}
+                    </p>
                 </div>
             </div>
         </div>
     );
-}
+};
+
+
+// function OrderComponent(order: Order) {
+//     return (
+//         <div className="mt-7 border border-gray-300 pt-9">
+//             <div className="flex max-md:flex-col items-center justify-between px-3 md:px-11">
+//                 <div className="data">
+//                     <p className="font-medium text-lg leading-8 text-black whitespace-nowrap">Order : #123</p>
+//                     <p className="font-medium text-lg leading-8 text-black mt-3 whitespace-nowrap">Order Payment : 18th March 2021</p>
+//                 </div>
+//                 <div className="flex items-center gap-3 max-md:mt-5">
+//                     <button className="rounded-full px-7 py-3 bg-white text-gray-900 border border-gray-300 font-semibold text-sm shadow-sm transition-all duration-500 hover:shadow-gray-200 hover:bg-gray-50 hover:border-gray-400">Show Invoice</button>
+//                     <button className="rounded-full px-7 py-3 bg-indigo-600 shadow-sm text-white font-semibold text-sm transition-all duration-500 hover:shadow-indigo-400 hover:bg-indigo-700">Buy Now</button>
+//                 </div>
+//             </div>
+//             <ProductInOrder />
+//             <div className="flex max-lg:flex-col items-center gap-8 lg:gap-24 px-3 md:px-11">
+//                 <div className="px-3 md:px-11 flex items-center justify-between max-sm:flex-col-reverse">
+//                     <div className="flex max-sm:flex-col-reverse items-center">
+//                         <button className="flex items-center gap-3 py-10 pr-8 sm:border-r border-gray-300 font-normal text-xl leading-8 text-gray-500 group transition-all duration-500 hover:text-indigo-600">
+//                             <svg width="40" height="41" viewBox="0 0 40 41" fill="none" xmlns="http://www.w3.org/2000/svg">
+//                                 <path className="stroke-gray-600 transition-all duration-500 group-hover:stroke-indigo-600" d="M14.0261 14.7259L25.5755 26.2753M14.0261 26.2753L25.5755 14.7259" stroke="" strokeWidth="1.8" strokeLinecap="round" strokeLinejoin="round" />
+//                             </svg>
+//                             Cancel Order
+//                         </button>
+//                         <p className="font-normal text-xl leading-8 text-gray-500 sm:pl-8">Payment Is Successful </p>
+//                     </div>
+//                     <p className="font-medium text-xl leading-8 text-black max-sm:py-4"><span className="text-gray-500"> Total Price: </span>&nbsp;$200.00</p>
+//                 </div>
+//             </div>
+//         </div>
+//     );
+// }
 
 function ProductInOrder() {
     return (
