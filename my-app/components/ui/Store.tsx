3649bd015ed6501a0d1ff30a62a03e1b2b205e0c
EfratYi <144124811+EfratYi@users.noreply.github.com>
2024-10-29 15:29:28 +0200

    catgories

--- my-app/components/ui/Store.tsx
+++ my-app/components/ui/Store.tsx
@@ -1,66 +1,36 @@
 "use client";
 import { useState, useEffect } from "react";
 import React from 'react'
-import Product from './Product';
+
 import FilterProduct from './FilterProduct';
 import axios from "axios"
+import Product from "@/components/ui/Product";
 
-
-export default function Store() {
-  const [products, setProducts] = useState([]);
-
-  type categoryAndSubId ={
+export type categoryAndSubId ={
     categoryId: string,
     subCategoryId: string
   }
-  // const products = [
-  //   {
-  //     id: '1',
-  //     name: 'Sole Elegance',
-  //     description: '5 types of shoes available',
-  //     price: 10.5,
-  //     imageUrl: 'https://readymadeui.com/images/product9.webp',
-  //   },
-  //   {
-  //     id: '2',
-  //     name: 'Urban Sneakers',
-  //     description: '5 types of shoes available',
-  //     price: 12.5,
-  //     imageUrl: 'https://readymadeui.com/images/product10.webp',
-  //   },
-  //   {
-  //     id: '3',
-  //     name: 'Velvet Boots',
-  //     description: '5 types of shoes available',
-  //     price: 14.5,
-  //     imageUrl: 'https://readymadeui.com/images/product11.webp',
-  //   },
-  //   {
-  //     id: '4',
-  //     name: 'Summit Hiking',
-  //     description: '5 types of shoes available',
-  //     price: 12.5,
-  //     imageUrl: 'https://readymadeui.com/images/product12.webp',
-  //   },
-  // ];
-  // useEffect(() => {
-  //   const fetchProducts = async (categoryAndSubId: categoryAndSubId) => {
-  //     console.log(categoryAndSubId);
-  //     try {
-  //       const { data } = await axios.get(`/api/filteredProducts/${categoryAndSubId}`);
-  //       setProducts(data.filteredProducts);
-  //       console.log(data.filteredProducts);
-  //     } catch (error) {
-  //       console.error("Error fetching subcategories:", error);
-  //     }};
-  //   fetchProducts("");
-  // }, []);
-  
+
+  export default function Store({ categoryIdAndSubId }: { categoryIdAndSubId: categoryAndSubId }) {
+    const [products, setProducts] = useState<any[]>([]);
+
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      console.log(categoryIdAndSubId);
+      try {
+        const { data } = await axios.get(`/api/filteredProducts/${categoryIdAndSubId}`);
+        setProducts(data.filteredProducts);
+        console.log(data.filteredProducts);
+      } catch (error) {
+        console.error("Error fetching subcategories:", error);
+      }};
+    fetchProducts();
+  }, []);
   
   return (
     <>
    <div className="font-[sans-serif] py-4 mx-auto lg:max-w-7xl sm:max-w-full">
-        <h2 className="text-4xl font-extrabold text-gray-800 mb-12">Premium Sneakers</h2>
         <div className="flex">
             {/* Filter Product on the left side */}
           <div className="lg:w-1/4 w-full">
@@ -70,9 +40,9 @@ export default function Store() {
           {/* Products Grid on the right side */}
           <div className="lg:w-3/4 w-full">
           <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4">
-          {/* {products.map((product) => (
-                // <Product key={product.id} product={product} />
-              ))} */}
+          {products.map((product) => (
+                <Product key={product.id} product={product} />
+              ))}
             </div>
           </div>
         </div>
--- my-app/components/ui/SubcategoryMenu.tsx
+++ my-app/components/ui/SubcategoryMenu.tsx
@@ -1,26 +1,35 @@
 
 "use client"
+import Link from "next/link";
 
-function SubcategoryMenu({ subcategories }: { subcategories: { name: string; category: string; items: string[] }[] }) {
-    return (
-        <>
-            <div className="flex gap-4">
-                <div className="mb-2 text-sm font-medium">
-                    <ul className="absolute left-0 mt-1 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-10">
-
-                        {subcategories.map((subcategory, index) => (
-                            <li key={index} className="px-4 py-2 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600">
-                                <a href={`/products/${subcategory}`} className="text-black dark:text-white block">
-                                    {subcategory.name}
-                                </a>
-                            </li>
-                        ))}
-                    </ul>
-                </div>
-            </div>
-        </>
-    );
-
+function SubcategoryMenu({ subcategories , categoryName}: { subcategories: {id:string; name: string; categoryId: string; items: string[] }[] ,categoryName: string}) {
+  console.log(subcategories)
+ 
+  return (
+    <div className="flex gap-4">
+      <div className="mb-2 text-sm font-medium">
+        <ul className="absolute left-0 mt-1 w-48 bg-white border border-gray-200 rounded-lg shadow-lg z-10">
+          {subcategories.map((subcategory, index) => {
+            // const categoryIdAndSubId = {
+            //     categoryId: subcategory.categoryId,
+            //     subcategoryId:subcategory.id 
+            //   };
+            return (
+              <li
+                key={index}
+                className="px-4 py-2 hover:bg-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600"
+              >
+                <Link href={`http://localhost:3000/products/${categoryName}/${subcategory.name}`} className="text-black dark:text-white block">
+                  {subcategory.name}
+                </Link>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </div>
+  );
 }
+
 
 export default SubcategoryMenu;
