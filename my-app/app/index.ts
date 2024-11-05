26fb5ed6f0ddafbf737c11ecbbdb885312652d82
michalw100 <m0583295188@gmail.com>
2024-11-05 15:31:08 +0200

    avatar

--- my-app/app/index.ts
+++ my-app/app/index.ts
@@ -1,10 +1,9 @@
-import { configureStore } from '@reduxjs/toolkit';
-import userReducer from '../slices/userSlice'; 
+// import { configureStore } from '@reduxjs/toolkit';
+// userReducer
+// const store = configureStore({
+//   reducer: {
+//     user: userReducer,
+//   },
+// });
 
-const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-});
-
-export default store;
+// export default store;
