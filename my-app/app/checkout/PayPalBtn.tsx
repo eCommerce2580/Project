d13c710e3b37944361866152a7f48859b3afb682
hadassaBrad <hadassa26162@gmail.com>
2024-11-06 23:31:15 +0200

    fix paypal btn

--- my-app/app/checkout/PayPalBtn.tsx
+++ my-app/app/checkout/PayPalBtn.tsx
@@ -1,34 +1,34 @@
-"use client"
+"use client";
 import {
   PayPalButtons,
   PayPalButtonsComponentProps,
 } from "@paypal/react-paypal-js";
-import { useUserStore } from '@/providers/userStore';
+import { useUserStore } from "@/providers/userStore";
 import axios from "axios";
-import { useDeliveryDetailsStore } from '@/providers/deliveryDetailsStrore'; 
-import {useCartStore} from '@/providers/cartStore'
+import { useDeliveryDetailsStore } from "@/providers/deliveryDetailsStrore";
+import { useCartStore } from "@/providers/cartStore";
 import { useRouter } from "next/navigation";
+import { useEffect } from "react";
 
- function PayPalBtn() {
-const  {deliveryDetails} =useDeliveryDetailsStore();
-const {cart,setCart}=useCartStore();
-const router = useRouter()
-const { user, setUser } = useUserStore();
-let adressId="0";
-if(user?.address?.addressId){
-  adressId=user.address.addressId;}
+function PayPalBtn() {
+  const { deliveryDetails } = useDeliveryDetailsStore();
+  const { cart, setCart } = useCartStore();
+  const router = useRouter();
+  const { user, setUser } = useUserStore();
+  const addressId = user?.address?.addressId;
+
   const createOrder: PayPalButtonsComponentProps["createOrder"] = async () => {
     try {
-   
+      console.log("user", user);
+
       const { data } = await axios({
         url: "/api/payment/createOrder",
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        data:JSON.stringify({
-          cart:cart//לבדוק מה באמת המבנה של עגלה, והאם האוביקט עצמו מביא את כל המוצרים
-        })
+        data: JSON.stringify({
+          cart: cart, //לבדוק מה באמת המבנה של עגלה, והאם האוביקט עצמו מביא את כל המוצרים
+        }),
       });
-
       return data.id;
     } catch (error) {
       console.error(error);
@@ -38,21 +38,29 @@ if(user?.address?.addressId){
 
   const onApprove: PayPalButtonsComponentProps["onApprove"] = async (d) => {
     // Capture the funds from the transaction.
-   try{const { data } = await axios({
-    url: "/api/payment/capturePayment",
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    data: JSON.stringify({ id:d.orderID,deliveryDetails,cart,adressId }),
-  });} catch(error) {
-    console.error(error);
-     throw error;
-  }
-  localStorage.setItem("cart","");
-  setCart([]);
 
-   alert(`Transaction completed by`);
-   
-   router.push("/userOrder")
+    try {
+      const { data } = await axios({
+        url: "/api/payment/capturePayment",
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        data: JSON.stringify({
+          id: d.orderID,
+          deliveryDetails,
+          cart,
+          addressId,
+        }),
+      });
+    } catch (error) {
+      console.error(error);
+      throw error;
+    }
+    localStorage.setItem("cart", "");
+    setCart([]);
+
+    alert(`Transaction completed by`);
+
+    router.push("/userOrders");
   };
 
   const styles: PayPalButtonsComponentProps["style"] = {
@@ -61,12 +69,15 @@ if(user?.address?.addressId){
   };
 
   return (
-<>{  deliveryDetails?.isComplited&& 
-    <PayPalButtons
-      style={styles}
-      createOrder={createOrder}
-      onApprove={onApprove}
-    />}</>   
+    <>
+      {deliveryDetails?.isComplited && (
+        <PayPalButtons
+          style={styles}
+          createOrder={createOrder}
+          onApprove={onApprove}
+        />
+      )}
+    </>
   );
 }
 
