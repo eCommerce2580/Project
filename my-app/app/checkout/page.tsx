f37d4c1407b8e29a06803684cb42cee473f9a990
michalw100 <m0583295188@gmail.com>
2024-11-07 12:04:42 +0200

    div in the page

--- my-app/app/checkout/page.tsx
+++ my-app/app/checkout/page.tsx
@@ -1,14 +1,12 @@
-
 import React from "react";
 import DeliveryDetails from "./DeliveryDetails";
 import PayPalBtn from "./PayPalBtn";
 
 export default function Checkout() {
-
   return (
-    <section className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16">
-    <  DeliveryDetails />
-    <  PayPalBtn />
-    </section>
+    <div className="bg-white py-8 antialiased dark:bg-gray-900 md:py-16 h-screen">
+      <DeliveryDetails />
+      <PayPalBtn />
+    </div>
   );
 }
