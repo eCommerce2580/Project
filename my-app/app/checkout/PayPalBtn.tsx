c07cb2919368366dd8839ff7b38d296243895d2c
AvitalWalden <A0583295187@GMAIL.COM>
2024-11-07 12:39:11 +0200

    pay

--- my-app/app/checkout/PayPalBtn.tsx
+++ my-app/app/checkout/PayPalBtn.tsx
@@ -9,14 +9,14 @@ import { useDeliveryDetailsStore } from "@/providers/deliveryDetailsStrore";
 import { useCartStore } from "@/providers/cartStore";
 import { useRouter } from "next/navigation";
 import { useTheme } from "next-themes";
-import { useEffect, useState } from "react";
 
 function PayPalBtn() {
   const { deliveryDetails } = useDeliveryDetailsStore();
   const { cart, setCart } = useCartStore();
   const router = useRouter();
-  const { user, setUser } = useUserStore();
+  const { user } = useUserStore();
   const addressId = user?.address?.addressId;
+  const { theme } = useTheme();
 
   const createOrder: PayPalButtonsComponentProps["createOrder"] = async () => {
     try {
@@ -54,25 +54,11 @@ function PayPalBtn() {
     layout: "vertical",
     color: theme === "dark" ? "silver" : "blue",
     height: 40,
-    color: theme === "dark" ? "silver" : "blue",
-    height: 40,
   };
 
   return (
     <>
       {deliveryDetails?.isComplited && (
-        <div
-          className={`p-6 rounded-lg shadow-lg flex justify-center ${
-            theme === "dark" ? "bg-gray-900 text-white" : ""
-          }`}
-        >
-          <PayPalButtons
-            style={styles}
-            createOrder={createOrder}
-            onApprove={onApprove}
-            forceReRender={[theme]}
-          />
-        </div>
         <div
           className={`p-6 rounded-lg shadow-lg flex justify-center ${
             theme === "dark" ? "bg-gray-900 text-white" : ""
@@ -91,4 +77,3 @@ function PayPalBtn() {
 }
 
 export default PayPalBtn;
-
