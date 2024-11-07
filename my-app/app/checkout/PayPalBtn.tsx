1b07cfd1a7c89984bca835a1a17451c2b4c9f0a4
estiww <esti69771@gmail.com>
2024-11-07 12:21:52 +0200

    sort

--- my-app/app/checkout/PayPalBtn.tsx
+++ my-app/app/checkout/PayPalBtn.tsx
@@ -1,46 +1,33 @@
-"use client"
+"use client";
 import {
   PayPalButtons,
   PayPalButtonsComponentProps,
 } from "@paypal/react-paypal-js";
-import { useUserStore } from '@/providers/userStore';
+import { useUserStore } from "@/providers/userStore";
 import axios from "axios";
-import { useDeliveryDetailsStore } from '@/providers/deliveryDetailsStrore';
-import { useCartStore } from '@/providers/cartStore'
+import { useDeliveryDetailsStore } from "@/providers/deliveryDetailsStrore";
+import { useCartStore } from "@/providers/cartStore";
 import { useRouter } from "next/navigation";
 import { useEffect } from "react";
 
-<<<<<<< HEAD
 function PayPalBtn() {
   const { deliveryDetails } = useDeliveryDetailsStore();
-  const { cart } = useCartStore();
-  const router = useRouter()
+  const { cart, setCart } = useCartStore();
+  const router = useRouter();
   const { user, setUser } = useUserStore();
   const addressId = user?.address?.addressId;
- 
-=======
- function PayPalBtn() {
-const  {deliveryDetails} =useDeliveryDetailsStore();
-const {cart,setCart}=useCartStore();
-const router = useRouter()
-const { user, setUser } = useUserStore();
-let adressId="0";
-if(user?.address?.addressId){
-  adressId=user.address.addressId;}
->>>>>>> ff38068fa9070c6da37efeac32ecc7a2f869656b
+
   const createOrder: PayPalButtonsComponentProps["createOrder"] = async () => {
     try {
-      
-      console.log("user", user)
-
+      console.log("user", user);
 
       const { data } = await axios({
         url: "/api/payment/createOrder",
         method: "POST",
         headers: { "Content-Type": "application/json" },
         data: JSON.stringify({
-          cart: cart//לבדוק מה באמת המבנה של עגלה, והאם האוביקט עצמו מביא את כל המוצרים
-        })
+          cart: cart, //לבדוק מה באמת המבנה של עגלה, והאם האוביקט עצמו מביא את כל המוצרים
+        }),
       });
       return data.id;
     } catch (error) {
@@ -51,35 +38,29 @@ if(user?.address?.addressId){
 
   const onApprove: PayPalButtonsComponentProps["onApprove"] = async (d) => {
     // Capture the funds from the transaction.
-<<<<<<< HEAD
+
     try {
       const { data } = await axios({
         url: "/api/payment/capturePayment",
         method: "POST",
         headers: { "Content-Type": "application/json" },
-        data: JSON.stringify({ id: d.orderID, deliveryDetails, cart, addressId }),
+        data: JSON.stringify({
+          id: d.orderID,
+          deliveryDetails,
+          cart,
+          addressId,
+        }),
       });
     } catch (error) {
       console.error(error);
       throw error;
     }
-=======
-   try{const { data } = await axios({
-    url: "/api/payment/capturePayment",
-    method: "POST",
-    headers: { "Content-Type": "application/json" },
-    data: JSON.stringify({ id:d.orderID,deliveryDetails,cart,adressId }),
-  });} catch(error) {
-    console.error(error);
-     throw error;
-  }
-  localStorage.setItem("cart","");
-  setCart([]);
->>>>>>> ff38068fa9070c6da37efeac32ecc7a2f869656b
+    localStorage.setItem("cart", "");
+    setCart([]);
 
     alert(`Transaction completed by`);
 
-    router.push("/userOrders")
+    router.push("/userOrders");
   };
 
   const styles: PayPalButtonsComponentProps["style"] = {
@@ -88,12 +69,15 @@ if(user?.address?.addressId){
   };
 
   return (
-    <>{deliveryDetails?.isComplited &&
-      <PayPalButtons
-        style={styles}
-        createOrder={createOrder}
-        onApprove={onApprove}
-      />}</>
+    <>
+      {deliveryDetails?.isComplited && (
+        <PayPalButtons
+          style={styles}
+          createOrder={createOrder}
+          onApprove={onApprove}
+        />
+      )}
+    </>
   );
 }
 
--- my-app/components/ui/Store.tsx
+++ my-app/components/ui/Store.tsx
@@ -16,26 +16,6 @@ export default function Store({
 }: {
   categoryIdAndSubId: categoryAndSubId;
 }) {
-  const [products, setProducts] = useState<any[]>([]);
-
-  const fetchProducts = async () => {
-    const { category, subCategory } = categoryIdAndSubId;
-
-    try {
-      const { data } = await axios.get(
-        `/api/filteredProducts?category=${category}&subCategory=${subCategory}`
-      );
-      
-      setProducts(data.filteredProducts);
-      console.log(data.filteredProducts);
-    } catch (error) {
-      console.error("Error fetching subcategories:", error);
-    }
-  };
-  useEffect(() => {
-    fetchProducts();
-  }, [categoryIdAndSubId]);
-
   return (
     <div className="min-h-screen bg-gray-50 dark:bg-gray-900">
       <div className="w-full">
